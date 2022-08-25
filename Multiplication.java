8b6d46e856b27a1442c770e4c5703b3bca6743b0
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-08-25 15:40:53 +0530

    import java.util.*; class classname {     public static void main(String x[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=sc.nextInt();         System.out.println(a*b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class classname
+{
+    public static void main(String x[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a*b);
+    }
+}
