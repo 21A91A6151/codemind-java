d8272b0fe6461d159b91ecb1b17230ca7c9e2f04
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-08-26 12:04:08 +0530

    import java.util.Scanner; class Triangle { public static void main(String x[]) {
    Scanner sc=new Scanner(System.in);
    float a,b;
    a=sc.nextFloat();
    b=sc.nextFloat();
    System.out.printf("%.2f",a*b);
    }
    
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Triangle
+{
+public static void main(String x[])
+{
+
+Scanner sc=new Scanner(System.in);
+float a,b;
+a=sc.nextFloat();
+b=sc.nextFloat();
+System.out.printf("%.2f",a*b);
+}
+
+}
