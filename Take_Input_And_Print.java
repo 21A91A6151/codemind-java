7a99470429b90371e4c8d6ed932d72feab5cef05
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-08-26 12:07:38 +0530

    import java.util.Scanner; class Triangle { public static void main(String x[]) {
    Scanner sc=new Scanner(System.in);
    String a;
    a=sc.nextLine();
    System.out.println(a);
    }
    
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Triangle
+{
+public static void main(String x[])
+{
+
+Scanner sc=new Scanner(System.in);
+String a;
+a=sc.nextLine();
+System.out.println(a);
+}
+
+}
