ce0f3d146995aee8cc42f344673012b75974b425
21A91A6151 <97425133+21A91A6151@users.noreply.github.com>
2022-09-28 17:59:59 +0530

    import java.util.Scanner; class classname {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j,k=64;         n=sc.nextInt();         for(i=n;i>0;i--)         {             for(j=i;j>0;j--)             {                 System.out.print((char)(k+i)+" ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,13 +4,13 @@ class classname
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,i,j;
+        int n,i,j,k=64;
         n=sc.nextInt();
-        for(i=1;i<=n;i++)
+        for(i=n;i>0;i--)
         {
-            for(j=1;j<=(n+1)-i;j++)
+            for(j=i;j>0;j--)
             {
-                System.out.print(j);
+                System.out.print((char)(k+i)+" ");
             }
             System.out.println();
         }
